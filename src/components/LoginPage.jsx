1f72b7779c2520e185b235a8bf151516b381d927
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-05-07 17:28:38 +0200

    Refactor firebase-config.js to export db and auth as named exports Update WatchlistPage.css to add styles for deleting movies in the watchlist

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,29 +1,42 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import './LoginPage.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword, setPersistence, browserLocalPersistence, onAuthStateChanged } from 'firebase/auth';
+import './LoginPage.css';
 import { auth } from '../firebase-config';
 
 export function LoginPage({ setUserConnected }) {
 
+  setPersistence(auth, browserLocalPersistence)
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigation = useNavigate();
+  const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (user) {
+        setUserConnected(true);
+        navigate('/');
+      } else {
+        setUserConnected(false);
+      }
+    });
+    return () => unsubscribe();
+  }, [navigate, setUserConnected]);
 
-    signInWithEmailAndPassword(auth, email, password)
-      .then(() => {
-        alert('Connexion réussie');
-        setUserConnected(true); // [2] Mettez à jour l'état pour indiquer que l'utilisateur est connecté
-        navigation('/');
-      })
-      .catch(error => {
-        setError(error.message);
-      });
-  }
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log('User logged in:', userCredential.user);
+      setUserConnected(true);
+      navigate('/');
+    } catch (error) {
+      console.error('Login error:', error);
+      setError(error.message);
+    }
+  };
 
   return (
     <div className="login">
@@ -31,20 +44,20 @@ export function LoginPage({ setUserConnected }) {
       <div className="form">
         <form onSubmit={handleLogin}>
           <div className="input">
-            <input 
-            type="email" 
-            id="email" 
-            placeholder='EMAIL' 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} />
+            <input
+              type="email"
+              id="email"
+              placeholder='EMAIL'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="input">
-            <input 
-            type="password" 
-            id="password" 
-            placeholder='MOT DE PASSE'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} /> 
+            <input
+              type="password"
+              id="password"
+              placeholder='MOT DE PASSE'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
           </div>
           <button type="submit">Se connecter</button>
           {error && <p className="error">{error}</p>}
--- src/components/WatchlistPage.jsx
+++ src/components/WatchlistPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { db, auth } from '../firebase-config';
-import { query, onSnapshot, orderBy, collection } from 'firebase/firestore'; // 'collection' removed if not used directly
-import { Link } from 'react-router-dom';
+import { query, onSnapshot, orderBy, collection, deleteDoc, doc } from 'firebase/firestore'; // 'collection' removed if not used directly
+// import { Link } from 'react-router-dom';
 import { FaPlay } from "react-icons/fa";
 import './WatchlistPage.css';
+import { RxCross2 } from "react-icons/rx";
 
 export function WatchlistPage() {
     const [watchlist, setWatchlist] = useState([]);
@@ -38,33 +39,54 @@ export function WatchlistPage() {
         return new Date(date).toLocaleDateString('fr-FR', options);
     }
 
+    const deleteMovie = async (movieId) => {
+        if (movieId !== 'string') {
+            let stringMovieId = movieId.toString();
+            const movieRef = doc(db, 'users', auth.currentUser.uid, 'watchlist', stringMovieId);
+            try {
+                await deleteDoc(movieRef);
+                alert('Film supprimé de votre watchlist');
+                setWatchlist(watchlist.filter(movie => movie.id !== movieId));
+            } catch (error) {
+                console.error('Erreur lors de la suppression du film de la watchlist :', error);
+            }
+        }
+    }
+
     return (
         <div className="watchlist">
             <h2>Ma Liste à visionner</h2>
             <ul>
                 {watchlist.map(movie => (
-                    <Link to={`/movie/${movie.id}`} key={movie.id}>
-                        <div className="moovie-container">
-                            <div className="left">
-                                <div className="card">
-                                    <li key={movie.id}>
-                                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                                    </li>
+                    <div key={movie.id} className="movie-container">
+                        <div className="left">
+                            <div className="card">
+                                <div className="delete-movies" onClick={(e) => {
+                                    e.stopPropagation(); // Arrête la propagation, mais n'est plus nécessaire ici
+                                    deleteMovie(movie.id);
+                                }}>
+                                    <RxCross2 />
                                 </div>
-                            </div>
-                            <div className="right">
-                                <div className="first-col">
-                                    <li className='title'>{movie.title || movie.name}</li>
-                                    <li>{formatDate(movie.release_date) || formatDate(movie.first_air_date)}</li>
-                                </div>
-                                <div className="second-col">
-                                    <a className='play-btn' href="youtube.com">
-                                        <FaPlay />
-                                    </a>
-                                </div>
+                                <li>
+                                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                                </li>
                             </div>
                         </div>
-                    </Link>
+                        <div className="right" onClick={() => {
+                            // Redirection programmée au clic sur la partie droite
+                            window.location.href = `/movie/${movie.id}`;
+                        }}>
+                            <div className="first-col">
+                                <li className='title'>{movie.title || movie.name}</li>
+                                <li>{formatDate(movie.release_date) || formatDate(movie.first_air_date)}</li>
+                            </div>
+                            <div className="second-col">
+                                <a className='play-btn' href="youtube.com">
+                                    <FaPlay />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </ul>
         </div>
