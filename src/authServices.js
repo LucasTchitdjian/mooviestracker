2ef65aa049a77a4409e98a8c86b11d95974da702
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-05-23 16:49:22 +0200

    added toast sucess pop up message when login out

--- src/authServices.js
+++ src/authServices.js
@@ -1,13 +1,23 @@
 import { signOut } from 'firebase/auth';
 import { auth } from './firebase-config';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const handleLogout = () => {
     signOut(auth)
         .then(() => {
-            alert('Vous êtes déconnecté');
+            toast.success("Vous êtes déconnecté", { autoClose: 3000 });
             window.location.reload();
         })
         .catch(error => {
             console.error('Erreur lors de la déconnexion :', error);
         });
+
+    return (
+        <div className="logout">
+            <ToastContainer />
+            <h1>Vous êtes déconnecté</h1>
+            <h3>Veuillez patientez vous allez être redirigé vers la page d'accueil</h3>
+        </div>
+    )
 }
