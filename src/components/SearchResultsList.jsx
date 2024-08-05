2e323836ffde39e2b58f0266a255a3fd70246e46
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-08-05 16:34:18 +0200

    feat: Revamp SearchResultsList component for real-time search functionality
    - Added state management for searchTerm and searchResults using useState.
    - Implemented useEffect to fetch search results from TMDB API based on searchTerm changes.
    - Updated search input to dynamically update searchTerm state on user input.
    - Removed the submit button from the search form for instant search results display.
    - Enhanced the handleAddToWatchlist function to support both movies and series.
    - Filtered menu items based on user connection status and visibility.
    - Improved layout and styling of search results with conditional rendering for vote_average.
    - Ensured smooth user experience with real-time updates and better state management.

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -1,6 +1,6 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './SearchResultsList.css';
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { FaPlus, FaStar, FaCheck } from "react-icons/fa";
 import { addToWatchlistMovies } from '../components/MooviesList';
 import { addToWatchlistSeries } from '../components/Series';
@@ -8,34 +8,29 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { GlobalContext } from '../context/GlobalContext';
 
-export function SearchResultsList({ movies, setMovies, searchTerm, setSearchTerm }) {
+export function SearchResultsList({ movies, setMovies }) {
+    //searchTerm Passe pas ce que j'ecrit dans la barre de recherche
 
     const { moviesAddedToWatchlist, setMoviesAddedToWatchlist, seriesAddedToWatchlist, setSeriesAddedToWatchlist } = useContext(GlobalContext);
-    const location = useLocation();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
-        const fetchSearchResults = async () => {
-            try {
-                const tmdbApiKey = process.env.REACT_APP_TMDB_API_KEY;
-                const query = new URLSearchParams(location.search).get('query');
-                if (!query) return;
-                setSearchTerm(query);
-                const response = await fetch(
-                    `https://api.themoviedb.org/3/search/multi?api_key=${tmdbApiKey}&query=${searchTerm}`
-                );
-                const data = await response.json();
-                setMovies(data.results);
-            } catch (error) {
-                console.error('Erreur lors de la recherche du film ou série:', error);
+        if (searchTerm) {
+            const fetchSearchResults = async () => {
+                try {
+                    const response = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=fr-FR&query=${searchTerm}&page=1&include_adult=false`);
+                    const data = await response.json();
+                    setSearchResults(data.results);
+                }
+                catch (error) {
+                    console.error(error);
+                }
             }
-        };
-        fetchSearchResults();
-    }, [location.search, setMovies, setSearchTerm, searchTerm]);
+            fetchSearchResults();
+        }
+    }, [searchTerm]);
 
-    // Fonction pour trier les films par vote_average décroissant
-    const sortMoviesByPopularity = (movies) => {
-        return movies.sort((a, b) => b.vote_average - a.vote_average); // Trier par ordre décroissant
-    };
 
     const handleAddToWatchlist = (item) => {
         if (item.type === 'movie') {
@@ -45,37 +40,36 @@ export function SearchResultsList({ movies, setMovies, searchTerm, setSearchTerm
         }
     };
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
     return (
         <div className="search-list">
             <ToastContainer />
-            <h2>Recherche</h2>
+            <div className="search-form">
+                <form className='search-form'>
+                    <input value={searchTerm} onChange={handleSearchChange} type="text" placeholder="Rechercher un film ou une série" />
+                </form>
+            </div>
             <ul>
-                {sortMoviesByPopularity(movies).map((moovie) => (
-                    <Link
-                        to={{
-                            pathname: `/search/${moovie.media_type}/${moovie.id}`,
-                            search: `?query=${searchTerm}`,
-                            state: { from: location }
-                        }}
-                        key={moovie.id}
-                    >
+                {searchResults.map((result) => (
+                    <Link to={`/search/${result.media_type}/${result.id}`} key={result.id}>
                         <div className="card">
                             <span onClick={(e) => {
                                 e.preventDefault();
-                                handleAddToWatchlist(moovie);
+                                handleAddToWatchlist(result);
                             }} className='add-watchlist'
-                                style={(moovie.type === 'movie' && moviesAddedToWatchlist.includes(moovie.id.toString())) || (moovie.media_type === 'tv' && seriesAddedToWatchlist.includes(moovie.id.toString())) ? { backgroundColor: '#22BB33' } : {}}>
-                                {(moovie.type === 'movie' && moviesAddedToWatchlist.includes(moovie.id.toString())) || (moovie.media_type === 'tv' && seriesAddedToWatchlist.includes(moovie.id.toString())) ? <FaCheck /> : <FaPlus />}
+                                style={(result.type === 'movie' && moviesAddedToWatchlist.includes(result.id.toString())) || (result.media_type === 'tv' && seriesAddedToWatchlist.includes(result.id.toString())) ? { backgroundColor: '#22BB33' } : {}}>
+                                {(result.type === 'movie' && moviesAddedToWatchlist.includes(result.id.toString())) || (result.media_type === 'tv' && seriesAddedToWatchlist.includes(result.id.toString())) ? <FaCheck /> : <FaPlus />}
+                                { /* Affichez l'icône appropriée en fonction de l'état de la liste de suivi */}
                             </span>
                             <p className='rating'>
-                                {moovie.vote_average !== undefined ? (
-                                    <>
-                                        <FaStar /> {moovie.vote_average.toFixed(1).replace('.', ',')}
-                                    </>
-                                ) : 'N/A'} {/* Afficher N/A si vote_average est indéfini */}
+                                <FaStar /> {result.vote_average ? result.vote_average.toFixed(1).replace('.', ',') : 'N/A'}
                             </p>
-                            <img src={`https://image.tmdb.org/t/p/w500${moovie.poster_path}`} alt="" />
+                            <img src={`https://image.tmdb.org/t/p/w500${result.poster_path}`} alt={result.title || result.name} />
                             <div className="moovie-info">
+                                <p>{result.title || result.name}</p>
                             </div>
                         </div>
                     </Link>
