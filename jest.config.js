8fe223c9855284ce09b6aa516b11b060d5cca19b
Lucas Tchitdjian <tchitdjian.lucas@gmail.com>
2024-08-08 11:42:13 +0200

    - Added jest.config.js for custom Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(css|scss)$': 'identity-obj-proxy',
+    },
+  };
+  
